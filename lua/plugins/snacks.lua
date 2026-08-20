d8fc3cccb5b1d796070da297b88a89ba381b17eb
Abdur Sharif <abdur.sharif@squarepoint-capital.com>
2026-08-20 19:53:20 +0100

    Overwrite snack dashboard keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,6 +9,16 @@ return {
     picker = { enabled = true },
     dashboard = {
       enabled = true,
+      preset = {
+        keys = {
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "n", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "p", desc = "Restore Plenary Session", section = "session" },
+          { icon = " ", key = "g", desc = "Grep Search", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
       sections = {
         { section = "header" },
         { section = "keys", gap = 1, padding = 1 },
